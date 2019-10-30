d232767a92f715198e2d4e97518b7eb67c74222f
20Bak <back.to.bak@gmail.com>
2019-10-30 14:15:27 +0100

    Setting up the envirenment

---
+++ src/test/java/fr/lacombe/AppTest.java
@@ -0,0 +1,19 @@
+package fr.lacombe;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assertions.assertThat( true ).isTrue();
+    }
+}
